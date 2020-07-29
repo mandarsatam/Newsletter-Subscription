df6f44fffb278be76fec35f7fcd40a7cdc8c58fa
soham <sohampawar161297@gmail.com>
2020-07-29 20:31:15 +0530

    fixed Error

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.post("/failure", function(req, res) {
     res.redirect("/");
 })
 
-app.listen(3000, function(req, res) {
+app.listen(process.env.PORT || 3000, function(req, res) {
     console.log("Server is running");
 
 });
